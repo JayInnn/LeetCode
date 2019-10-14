f5f494e4a9ae78a1447b67434c83415dda0316aa
jayinnn <475660997@qq.com>
2019-10-14 19:50:51 +0800

    hello world

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * @author: yinwenjie
+ * @email: 475660997@qq.com
+ * @date: 2019/10/14
+ * @description:
+ * @result:
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
