d9373b40d8fdeb2bbd9f584c296158433ccdd943
jayinnn <475660997@qq.com>
2019-10-14 20:18:00 +0800

    exe01--two sum

---
+++ src/Chapter1/Exe01.java
@@ -0,0 +1,26 @@
+package Chapter1;
+
+import java.util.HashMap;
+
+/**
+ * @author: yinwenjie
+ * @email: 475660997@qq.com
+ * @date: 2019/10/14
+ * @description:
+ * @result:
+ */
+public class Exe01 {
+    public int[] twoSum(int[] nums, int target) {
+        int[] index = new int[2];
+        int size = (new Double(nums.length / 0.75)).intValue();
+        HashMap<Integer, Integer> hashMap = new HashMap<>(size);
+        for (int i = 0; i < nums.length; i++){
+            if (hashMap.containsKey(nums[i])){
+                index[0] = i;
+                index[1] = hashMap.get(nums[i]);
+            }
+            hashMap.put(target-nums[i], nums[i]);
+        }
+        return index;
+    }
+}
