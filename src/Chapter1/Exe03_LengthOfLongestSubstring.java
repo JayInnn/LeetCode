a383d751c8a834ecb09780a120802c70e9c5dd9b
jayinnn <475660997@qq.com>
2019-10-15 16:34:48 +0800

    length of longest sub-string

--- src/Chapter1/Exe03_LengthOfLongestSubstring.java
+++ src/Chapter1/Exe03_LengthOfLongestSubstring.java
@@ -8,4 +8,21 @@ package Chapter1;
  * @result:
  */
 public class Exe03_LengthOfLongestSubstring {
+    public int lengthOfLongestSubstring(String s) {
+        char[] chars = s.toCharArray();
+        int k = 0, max = 0;
+        for (int i = 0; i < s.length(); i++){
+            for (int j = k; j < i; j++)
+                if (chars[j] == chars[i]){
+                    k = j + 1;
+                    break;
+                }
+            max = max > (i - k + 1) ? max : (i - k + 1);
+        }
+        return max;
+    }
+
+    public static void main(String[] args){
+        System.out.println(new Exe03_LengthOfLongestSubstring().lengthOfLongestSubstring("abcabcbb"));
+    }
 }
