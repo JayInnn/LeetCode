8a4ba00a64ed8cd6e9e6f3183d145cb8f943f0bc
jayinnn <475660997@qq.com>
2019-10-29 10:29:04 +0800

    exe25 -- reverse k group

--- src/Chapter1/Exe25_ReverseKGroup.java
+++ src/Chapter1/Exe25_ReverseKGroup.java
@@ -54,8 +54,7 @@ public class Exe25_ReverseKGroup {
         ListNode c = new ListNode(3);
         ListNode d = new ListNode(4);
         ListNode e = new ListNode(5);
-        a.next = b;
-        b.next = c; c.next = d; d.next = e;
+        a.next = b;b.next = c; c.next = d; d.next = e;
         System.out.println(new Exe25_ReverseKGroup().reverseKGroup(a, 2));
     }
 }
